9b7d049b04c1992c91bc9e78e37dd8c040078e8d
예지 <lee24pm@gmail.com>
2024-12-17 00:28:43 +0900

    Feat: 4. DB 접근 최소화

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,11 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
 
+        List<User> users = userRepository.findAllById(userIds);
+
+        for (User user : users) {
             user.updateStatusToBlocked();
-
-            userRepository.save(user);
         }
     }
 }
