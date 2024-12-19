4f88e61066d06c2db2d128b80d29fa9da6aa210c
예지 <lee24pm@gmail.com>
2024-12-19 15:06:41 +0900

    Feat: 8-2. Item Entity Test 추가하기

---
+++ src/test/java/com/example/demo/entity/ItemTest.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity;
+
+import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+class ItemTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User owner;
+    private User manager;
+
+    @BeforeEach
+    void setUp() {
+        owner = userRepository.save(new User("user", "test@email.com", "nickname", "1234"));
+        manager = userRepository.save(new User("admin", "admin@email.com", "admin", "1234"));
+    }
+
+    @Test
+    void testgetStatus() {
+        //when
+        Item item = new Item("item", "description", manager, owner);
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        assertThat(savedItem.getStatus()).isEqualTo("PENDING");
+    }
+
+//    Item entity에 있는 @DynamicInsert으로 테스트 중단
+//    @Test
+//    void testStatusisNotNull() {
+//        //when
+//        Item item = new Item("item", "description", manager, owner, null);
+//
+//        //then
+//        assertThrows(DataIntegrityViolationException.class, ()-> {
+//            itemRepository.save(item);
+//        });
+//    }
+
+}
